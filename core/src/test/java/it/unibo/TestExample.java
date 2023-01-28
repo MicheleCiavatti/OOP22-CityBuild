fd023509ab57ecb5f25c263652b85d7da379a34a
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-28 15:40:37 +0100

    Setup Junit dependency and created TestExample

---
+++ core/src/test/java/it/unibo/TestExample.java
@@ -0,0 +1,13 @@
+package it.unibo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestExample {
+    
+    @Test
+    public void testExample() {
+        assertEquals(1, 1);
+    }
+}
