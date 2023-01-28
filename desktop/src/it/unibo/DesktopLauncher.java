28afcc034feb0903bad46add192a4d46839d7163
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-28 14:52:43 +0100

    Setup project

--- desktop/src/it/unibo/DesktopLauncher.java
+++ desktop/src/it/unibo/DesktopLauncher.java
@@ -2,6 +2,7 @@ package it.unibo;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 import it.unibo.CityBuild;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
