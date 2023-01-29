a781dd2b29032b9e9d4fa66da4ecaa7bbca91d22
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-29 23:18:06 +0100

    Initialized House

---
+++ core/src/main/java/it/unibo/model/impl/House.java
@@ -0,0 +1,27 @@
+package it.unibo.model.impl;
+
+import java.util.List;
+import it.unibo.model.api.Building;
+import it.unibo.model.api.Resource;
+
+public class House implements Building {
+
+    @Override
+    public int getResource() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<Resource> getCostUpgrade() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void upgrade(List<Resource> resourcesForUpgrade) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
