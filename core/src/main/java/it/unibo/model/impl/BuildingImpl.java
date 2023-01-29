aace99973e9451e0794130237bcfee63dfa07dc4
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-29 23:21:22 +0100

    Initialized BuildingImpl

---
+++ core/src/main/java/it/unibo/model/impl/BuildingImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.model.impl;
+
+import java.util.Map;
+import it.unibo.model.api.Building;
+import it.unibo.model.api.Resource;
+
+public class BuildingImpl implements Building {
+
+    @Override
+    public int getResource() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Map<Resource, Integer> getCostUpgrade() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void upgrade(Map<Resource, Integer> resourcesForUpgrade) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
