8b4c5cf917c388f8d5f5de98fd33341a73fef300
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-03 09:52:46 +0100

    Added javadoc to BuildingFactory

--- core/src/main/java/it/unibo/model/api/BuildingFactory.java
+++ core/src/main/java/it/unibo/model/api/BuildingFactory.java
@@ -1,6 +1,17 @@
 package it.unibo.model.api;
 
+/**Factory class for creating different kinds of {@link Building}.*/
 public interface BuildingFactory {
     
-    public ProductionBuilding getSimpleProductionBuilding(final Resource resource);
+    /**Returns an instance of a specific {@link ProductionBuilding}. 
+     * More specifically:
+     * WATER -> Depurator
+     * WOOD -> Woodcutter
+     * ENERGY -> Power Plant
+     * METAL -> Foundry
+     * CITIZEN -> House
+     * @param resource used to specify the building required
+     * @return a class implementing {@link ProductionBuilding}
+    */
+    public ProductionBuilding createSimpleProductionBuilding(final Resource resource);
 }
