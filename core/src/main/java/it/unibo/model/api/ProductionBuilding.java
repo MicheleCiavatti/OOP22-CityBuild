893611e190e7c4dc6eabef6d8417ea5ab5bf8f40
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-03 09:56:19 +0100

    Initialized javadoc for ProductionBuilding

--- core/src/main/java/it/unibo/model/api/ProductionBuilding.java
+++ core/src/main/java/it/unibo/model/api/ProductionBuilding.java
@@ -2,12 +2,16 @@ package it.unibo.model.api;
 
 import java.util.Map;
 
+/**This interface is used to Model any building that produces resources */
 public interface ProductionBuilding {
 
+    /** */
     public int getResource();
 
+    /** */
     public Map<Resource, Integer> getCostUpgrade();
     
+    /** */
     public void upgrade(final Map<Resource, Integer> resourcesForUpgrade);
 
 }
