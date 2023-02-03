25383ee5eeff60471178a03f678338c3adba2869
Anthony Dagos <jhonanthony.dagos@studio.unibo.it>
2023-02-03 23:39:44 +0100

    Changing name of ConstructionCostFactory class into SimpleConstructionCostFactory

--- core/src/main/java/it/unibo/model/impl/SimpleConstructionCostFactory.java
+++ core/src/main/java/it/unibo/model/impl/SimpleConstructionCostFactory.java
@@ -3,7 +3,7 @@ package it.unibo.model.impl;
 import it.unibo.model.api.Cost;
 import it.unibo.model.api.TypeBuilding;
 
-public class ConstructionCostFactory {
+public class SimpleConstructionCostFactory {
     public Cost getType(TypeBuilding type){
         if(type == null){
             return null;
@@ -12,7 +12,7 @@ public class ConstructionCostFactory {
             return new UpgradeCostImpl();
         }
         else if(type == TypeBuilding.FIRST_CONSTRUCTION){
-            return new ConstructionCostImpl();
+            return new SimpleConstructionCostImpl();
         }
         return null;
     }
