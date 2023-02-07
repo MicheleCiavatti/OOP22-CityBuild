de23e0e781ad845c1d9090aaa97591047cf9d2a4
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-07 09:35:25 +0100

    Attempt with ClassLoader to get relative path to resources

--- core/src/main/java/it/unibo/controller/impl/EconomyFileReaderImpl.java
+++ core/src/main/java/it/unibo/controller/impl/EconomyFileReaderImpl.java
@@ -1,7 +1,19 @@
 package it.unibo.controller.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import it.unibo.controller.api.EconomyFileReader;
+import it.unibo.model.api.Resource;
 
 public class EconomyFileReaderImpl implements EconomyFileReader {
+
+    private static final ClassLoader LOADER = EconomyFileReaderImpl.class.getClassLoader();
+
+    @Override
+    public List<Map<String, Integer>> getEconomyTables(Resource r) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
