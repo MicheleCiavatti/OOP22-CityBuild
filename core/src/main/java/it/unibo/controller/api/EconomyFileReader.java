1e539066ab833fcc56c9cf172ea0e81dc4a5fff7
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-07 11:38:38 +0100

    Added javadoc to EconomyFileReader

--- core/src/main/java/it/unibo/controller/api/EconomyFileReader.java
+++ core/src/main/java/it/unibo/controller/api/EconomyFileReader.java
@@ -2,10 +2,14 @@ package it.unibo.controller.api;
 
 import java.util.List;
 import java.util.Map;
-
 import it.unibo.model.api.Resource;
 
+/**This interface is used to read YAML files containing the economy tables (revenue, construction costs and upgrade costs) of buildings */
 public interface EconomyFileReader {
     
+    /**Returns a list containing the tables of a simple building
+     * @return a list with always 3 maps: the first is the revenue table, the second is the construction table and the third is the upgrade table
+     * @param r used to specify which simple building
+     */
     public List<Map<Resource, Integer>> getSimpleEconomyTables(final Resource r);
 }
