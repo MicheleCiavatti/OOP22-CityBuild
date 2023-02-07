5ff619fe9cc5cff51d47d3b566e79773580ccef9
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-07 11:45:20 +0100

    Added javadoc

--- core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
+++ core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
@@ -1,19 +1,20 @@
 package it.unibo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.Map;
-
 import org.junit.jupiter.api.Test;
-
 import it.unibo.controller.api.EconomyFileReader;
 import it.unibo.controller.impl.EconomyFileReaderImpl;
 import it.unibo.model.api.Resource;
 
+/**A class to test the functioning of {@link it.unibo.controller.api.EconomyFileReader} */
 public class TestEconomyFileReader {
 
     private final EconomyFileReader economyFileReader = new EconomyFileReaderImpl();
 
+    /**Testing the reading of the house.yml file.
+     * Note that the test fails if house.yml file is changed without changing the Maps in TestHouse.
+     */
     @Test
     public void testHouse() {
         var tables = economyFileReader.getSimpleEconomyTables(Resource.CITIZEN);
