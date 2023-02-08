9c1e4c2949cf15fe99f5db56bd5d40466dbbc5bd
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-08 11:22:37 +0100

    Added removeEmptyResources() to SimpleProductionBuilding

--- core/src/main/java/it/unibo/model/impl/SimpleProductionBuilding.java
+++ core/src/main/java/it/unibo/model/impl/SimpleProductionBuilding.java
@@ -1,45 +1,64 @@
 package it.unibo.model.impl;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 import it.unibo.model.api.EconomyHandler;
 import it.unibo.model.api.EconomyHandlerFactory;
 import it.unibo.model.api.ProductionBuilding;
 import it.unibo.model.api.Resource;
 
+/**The class is an implementation of {@link it.unibo.model.api.ProductionBuilding} that models only Simple buildings.
+ * The software has both Simple and Advanced building.
+ */
 public class SimpleProductionBuilding implements ProductionBuilding {
 
     private static final int MULTIPLIER = 2;
     private final Map<Resource, Integer> upgradeCost;
     private final Map<Resource, Integer> revenue;
     private final Map<Resource, Integer> constructionCost;
+    private boolean upgradable;
 
     public SimpleProductionBuilding(final Resource r) {
         EconomyHandlerFactory economyHandlerFactory = new EconomyHandlerFactoryImpl();
         EconomyHandler economyHandler = economyHandlerFactory.createEconomyHandler();
-        this.upgradeCost = Map.copyOf(economyHandler.getSimpleUpgradeTable(r));
-        this.revenue = Map.copyOf(economyHandler.getSimpleRevenueTable(r));
-        this.constructionCost = Map.copyOf(economyHandler.getSimpleCostTable(r));
+        this.upgradeCost = this.removeEmptyResources(economyHandler.getSimpleUpgradeTable(r));
+        this.revenue = this.removeEmptyResources(economyHandler.getSimpleRevenueTable(r));
+        this.constructionCost = this.removeEmptyResources(economyHandler.getSimpleCostTable(r));
+        this.upgradable = true;
     }
 
+    /**{@inheritDoc} */
     @Override
     public Map<Resource, Integer> getRevenue() {
         return Map.copyOf(this.revenue);
     }
 
+    /**{@inheritDoc} */
     @Override
     public Map<Resource, Integer> getCostUpgrade() {
         return Map.copyOf(this.upgradeCost);
     }
 
+        /**{@inheritDoc} */
     @Override
     public void upgrade(Map<Resource, Integer> resourcesForUpgrade) {
-        if (this.upgradeCost.equals(resourcesForUpgrade)) {
+        if (this.upgradeCost.equals(this.removeEmptyResources(resourcesForUpgrade)) && this.upgradable) {
             this.revenue.replaceAll((key, value) -> value * MULTIPLIER);
+            this.upgradable = false;
         }
     }
 
+    /**{@inheritDoc} */
     @Override
     public Map<Resource, Integer> getCostConstruction() {
         return Map.copyOf(this.constructionCost);
+    }
+
+    //A transformation so that keys with 0 value are removed
+    private Map<Resource, Integer> removeEmptyResources(final Map<Resource, Integer> m) {
+        return m.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() > 0)
+                .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
     }
 }
