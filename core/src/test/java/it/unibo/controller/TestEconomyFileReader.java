576e45e53074c7270825d238c256bd26d5b02735
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-08 10:59:18 +0100

    Removed unused imports in TestEconomyFileReader

--- core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
+++ core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
@@ -2,8 +2,6 @@ package it.unibo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import it.unibo.controller.api.EconomyFileReader;
 import it.unibo.controller.impl.EconomyFileReaderImpl;
