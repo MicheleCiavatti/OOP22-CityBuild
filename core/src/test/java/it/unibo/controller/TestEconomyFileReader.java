ee56588c3aee06bcd649d91a5a5250a4d973ec5c
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-08 10:58:46 +0100

    Changed testHouse()

--- core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
+++ core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
@@ -1,6 +1,8 @@
 package it.unibo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import it.unibo.controller.api.EconomyFileReader;
@@ -13,12 +15,12 @@ public class TestEconomyFileReader {
     private final EconomyFileReader economyFileReader = new EconomyFileReaderImpl();
 
     /**Testing the reading of the house.yml file.
-     * Note that the test fails if house.yml file is changed without changing the Maps in TestHouse.
+     * 
      */
     @Test
     public void testHouse() {
         var tables = economyFileReader.getSimpleEconomyTables(Resource.CITIZEN);
-        assertEquals(tables.get(0), Map.of(Resource.CITIZEN, 10));
-        assertEquals(tables.get(1), Map.of(Resource.METAL, 5, Resource.WOOD, 7));
+        assertNotNull(tables);
+        assertEquals(tables.size(), 3);
     }
 }
