46ad54422fc1a7ac8fe481c0d76c5543a3040031
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-08 11:13:59 +0100

    Added javadoc to ProductionBuilding

--- core/src/main/java/it/unibo/model/api/ProductionBuilding.java
+++ core/src/main/java/it/unibo/model/api/ProductionBuilding.java
@@ -2,19 +2,28 @@ package it.unibo.model.api;
 
 import java.util.Map;
 
-/**This interface is used to Model any building that produces resources */
+/**This interface is used to model any building that produces resources.*/
 public interface ProductionBuilding {
 
-    /** */
+    /**Returns the revenue of the building.
+     * @return a {@link java.util.Map} representing the revenue of the production building
+     */
     public Map<Resource, Integer> getRevenue();
 
-    /** */
+    /**Returns the upgrade cost of the building.
+     * @return a {@link java.util.Map} representing the upgrade cost of the production building
+     */
     public Map<Resource, Integer> getCostUpgrade();
 
-    /** */
+    /**Returns the construction cost of the building.
+     * @return a {@link java.util.Map} representing the construction cost of the production building
+     */
     public Map<Resource, Integer> getCostConstruction();
     
-    /** */
+    /**Upgrades the building. 
+     * @param resourcesForUpgrade the method makes a check to verify that the resources passed are appropriate 
+     * for upgrading the building
+     */
     public void upgrade(final Map<Resource, Integer> resourcesForUpgrade);
 
 }
