66a38d2fe1b8d2a4ea122749a9496c6568a51895
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-08 11:01:35 +0100

    Initialized EconomyHandlerFactoryImpl

---
+++ core/src/main/java/it/unibo/model/impl/EconomyHandlerFactoryImpl.java
@@ -0,0 +1,14 @@
+package it.unibo.model.impl;
+
+import it.unibo.model.api.EconomyHandler;
+import it.unibo.model.api.EconomyHandlerFactory;
+
+public class EconomyHandlerFactoryImpl implements EconomyHandlerFactory {
+
+    @Override
+    public EconomyHandler createEconomyHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
