3145d1e36123363e623a652a779effdd41a21cb5
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-08 11:47:00 +0100

    Added BuildingFactoryImpl

---
+++ core/src/main/java/it/unibo/model/impl/BuildingFactoryImpl.java
@@ -0,0 +1,16 @@
+package it.unibo.model.impl;
+
+import it.unibo.model.api.BuildingFactory;
+import it.unibo.model.api.ProductionBuilding;
+import it.unibo.model.api.Resource;
+
+/**An implementation of {@link it.unibo.model.api.BuildingFactory} */
+public class BuildingFactoryImpl implements BuildingFactory {
+
+    /**{@inheritDoc} */
+    @Override
+    public ProductionBuilding createSimpleProductionBuilding(final Resource resource) {
+        return new SimpleProductionBuilding(resource);
+    }
+    
+}
