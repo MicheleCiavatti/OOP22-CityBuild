8db9b98fee0e381a4b6d7bcba9f44631cb9adfd6
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-09 18:34:13 +0100

    Added javadoc to Player

--- core/src/main/java/it/unibo/model/api/Player.java
+++ core/src/main/java/it/unibo/model/api/Player.java
@@ -2,13 +2,29 @@ package it.unibo.model.api;
 
 import java.util.Map;
 
+/**This interface models the player in the game */
 public interface Player {
     
+    /**Returns an integer representing the quantity of the specific resource that the player possess.
+     * @param r resource requested
+     * @return an integer with the quantity associated to the resource
+     */
     public int getResourceAvailable(final Resource r);
 
+    /**Returns all resources available to the player.
+     * @return a {@link java.util.Map} containing all resources in possession of the player
+     */
     public Map<Resource, Integer> getAllResourcesAvailable();
 
+    /**Removes the specified resources to the player. 
+     * The method makes an internal check to verify that the resources we want to spend are available to the player
+     * @param toSpend a {@link java.util.Map} describing the resources we want to spend
+     * @return true if the player actually spent the resources (the parameter passes the internal check), false otherwise
+     */
     public boolean spendResources(final Map<Resource, Integer> toSpend);
 
+    /**Adds the requested resources to the player possessions.
+     * @param toAdd a {@link java.util.Map} describing the resources we want to add
+     */
     public void addResources(final Map<Resource, Integer> toAdd);
 }
