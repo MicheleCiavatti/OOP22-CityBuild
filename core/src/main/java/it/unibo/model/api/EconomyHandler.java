026e2112554ea164cd91fb4071dec964c66a1143
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-09 12:13:29 +0100

    Added 3 methods in EconomyHandler

--- core/src/main/java/it/unibo/model/api/EconomyHandler.java
+++ core/src/main/java/it/unibo/model/api/EconomyHandler.java
@@ -7,4 +7,10 @@ public interface EconomyHandler {
     public Map<Resource,Integer> getSimpleRevenueTable(final Resource resource);
     public Map<Resource,Integer> getSimpleUpgradeTable(final Resource resource);
 
+    public Map<Resource, Integer> getAdvancedCostTable(final Resource resource);
+
+    public Map<Resource, Integer> getAdvancedRevenueTable(final Resource resource);
+
+    public Map<Resource, Integer> getAdvancedUpgradeTable(final Resource resource);
+
 }
