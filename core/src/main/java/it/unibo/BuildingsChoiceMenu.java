318861b837a9c80438633fc935f0905f60cc6fa4
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-17 14:00:33 +0100

    created image button

--- core/src/main/java/it/unibo/BuildingsChoiceMenu.java
+++ core/src/main/java/it/unibo/BuildingsChoiceMenu.java
@@ -1,3 +1,35 @@
-public class BuildingsChoiceMenu {
+package it.unibo;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public class BuildingsChoiceMenu implements Screen {
+    private Stage stage;
+
+    public BuildingsChoiceMenu(){
+        stage = new Stage();
+        
+        Texture icon1Texture = new Texture("icon1.png");
+        TextureRegion icon1 = new TextureRegion(icon1Texture);
+
+        Texture icon2Texture = new Texture("icon2.png");
+        TextureRegion icon2 = new TextureRegion(icon1Texture);
+
+        Texture icon3Texture = new Texture("icon3.png");
+        TextureRegion icon3 = new TextureRegion(icon1Texture);
+
+        ImageButton button1 = new ImageButton(new TextureRegionDrawable(icon1));
+        ImageButton button2 = new ImageButton(new TextureRegionDrawable(icon2));
+        ImageButton button3 = new ImageButton(new TextureRegionDrawable(icon3));
+
+        
+        stage.addActor(button1);
+        stage.addActor(button2);
+        stage.addActor(button3);
+        
+    }
     
 }
