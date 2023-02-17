b4174c1ad1c2d75e753f6d7f42b352bdebb01dc7
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-17 12:22:51 +0100

    Added main menu background

--- desktop/src/it/unibo/DesktopLauncher.java
+++ desktop/src/it/unibo/DesktopLauncher.java
@@ -1,14 +1,17 @@
 package it.unibo;
 
+import java.awt.Toolkit;
+import java.awt.Dimension;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-
 import it.unibo.CityBuild;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		var size = Toolkit.getDefaultToolkit().getScreenSize();
+		config.setWindowedMode((int) size.getWidth(), (int) size.getHeight());
 		config.setForegroundFPS(60);
 		config.setTitle("CityBuild");
 		new Lwjgl3Application(new CityBuild(), config);
