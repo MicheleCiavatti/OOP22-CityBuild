2e5223e08e9f250a80dd13b647b549ccb4c4b012
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-17 10:36:24 +0100

    basic set class

--- core/src/main/java/it/unibo/BackgroundTask.java
+++ core/src/main/java/it/unibo/BackgroundTask.java
@@ -1,3 +1,24 @@
+package it.unibo;
+
+/*A class for executing specific procedures when application is 
+closed*/
+
+
 public class BackgroundTask {
+    private ScheduledExecutorService executorService;
+
+    public BackgroundTask() {
+        executorService = Executors.newSingleThreadExecutor();
+    }    
+
+    public void start(){
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                //TODO
+            }
+        };
+        }
+    }
     
 }
