b645390472bae1aac62c489715e1ea0838b6a676
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-17 16:41:46 +0100

    Added both buttons to main menù. Both buttons only print Button pressed for now

--- core/src/main/java/it/unibo/CityBuild.java
+++ core/src/main/java/it/unibo/CityBuild.java
@@ -15,28 +15,41 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class CityBuild extends ApplicationAdapter {
 	
-	public static int MENU_WIDTH = 1440;
-	public static int MENU_HEIGHT = 810;
-
-	private static float BUTTON_WIDTH = 300f;
-	private static float BUTTON_HEIGHT = 150f;
+	public static final int MENU_WIDTH = 1440;
+	public static final int MENU_HEIGHT = 810;
+	
+	private static final int SCREEN_DIVISOR = 12;
+	private static final float BUTTON_WIDTH = 300f;
+	private static final float BUTTON_HEIGHT = 150f;
 
 	private Stage stage;
 	
 	@Override
 	public void create () {
+		final int rowHeight = Gdx.graphics.getHeight() / SCREEN_DIVISOR;
 		this.stage = new Stage(new ScreenViewport());
-		Image background = new Image(new Texture("CityBuild.png"));
+		final Image background = new Image(new Texture("CityBuild.png"));
 		background.setName("Background");
 		background.setPosition(
 			Gdx.graphics.getWidth() / 2 - background.getWidth() / 2, 
 			Gdx.graphics.getHeight() / 2 - background.getHeight() / 2);
 		this.stage.addActor(background);
-
-		Skin skin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
-		Button newGame = new TextButton("New Game", skin);
+		final Skin skin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+		final Button newGame = new TextButton("New Game", skin);
+		final Button loadGame = new TextButton("Load Game", skin);
+		loadGame.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 		newGame.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-		newGame.setPosition(Gdx.graphics.getWidth() / 2 - BUTTON_WIDTH / 2, Gdx.graphics.getHeight() - BUTTON_HEIGHT);
+		/*loadGame in lower part of the screen. */
+		loadGame.setPosition(Gdx.graphics.getWidth() / 2 - BUTTON_WIDTH / 2, rowHeight);
+		/*newGame in upper part of the screen. */
+		newGame.setPosition(Gdx.graphics.getWidth() / 2 - BUTTON_WIDTH / 2, Gdx.graphics.getHeight() - BUTTON_HEIGHT - rowHeight);
+		loadGame.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("Button pressed");
+				return true;
+			} 
+		});
 		newGame.addListener(new InputListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -45,8 +58,8 @@ public class CityBuild extends ApplicationAdapter {
 			} 
 		});
 		this.stage.addActor(newGame);
+		this.stage.addActor(loadGame);
 		Gdx.input.setInputProcessor(this.stage);
-
 	}
 
 	@Override
