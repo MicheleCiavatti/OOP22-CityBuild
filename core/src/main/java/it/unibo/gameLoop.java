ee2f31222e5d71312ef971b71f6b38c1c2c4a7ba
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-17 10:58:22 +0100

    created gameloop class

---
+++ core/src/main/java/it/unibo/gameLoop.java
@@ -0,0 +1,7 @@
+package it.unibo;
+public class gameLoop {
+    public gameLoop(CityBuild cityBuild, BackgroundTask backgroundTask) {
+        this.cityBuild = cityBuild;
+        this.backgroundTask = backgroundTask;
+    }
+}
