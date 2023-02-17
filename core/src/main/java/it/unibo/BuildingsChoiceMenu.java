ba3bcc8154bb3f0872329fde28746e48b4ad0ef8
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-17 14:07:33 +0100

    set position and dimension buttons

--- core/src/main/java/it/unibo/BuildingsChoiceMenu.java
+++ core/src/main/java/it/unibo/BuildingsChoiceMenu.java
@@ -1,5 +1,6 @@
 package it.unibo;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -29,7 +30,20 @@ public class BuildingsChoiceMenu implements Screen {
         stage.addActor(button1);
         stage.addActor(button2);
         stage.addActor(button3);
-        
+
+        float buttonWidth = Gdx.graphics.getWidth() * 0.2f;
+        float buttonHeight = Gdx.graphics.getHeight() * 0.2f;
+        float spacing = Gdx.graphics.getWidth() * 0.1f;
+
+        button1.setPosition(spacing, spacing);
+        button1.setSize(buttonWidth, buttonHeight);
+
+        button2.setPosition(spacing, spacing);
+        button2.setSize(buttonWidth, buttonHeight);
+
+        button3.setPosition(spacing, spacing);
+        button3.setSize(buttonWidth, buttonHeight);
+
     }
     
 }
