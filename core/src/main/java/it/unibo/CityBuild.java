2b2007df59dc129f11ead9757271d2017f914228
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-17 16:31:16 +0100

    Basic button implemented

--- core/src/main/java/it/unibo/CityBuild.java
+++ core/src/main/java/it/unibo/CityBuild.java
@@ -3,8 +3,13 @@ package it.unibo;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -12,6 +17,9 @@ public class CityBuild extends ApplicationAdapter {
 	
 	public static int MENU_WIDTH = 1440;
 	public static int MENU_HEIGHT = 810;
+
+	private static float BUTTON_WIDTH = 300f;
+	private static float BUTTON_HEIGHT = 150f;
 
 	private Stage stage;
 	
@@ -24,6 +32,21 @@ public class CityBuild extends ApplicationAdapter {
 			Gdx.graphics.getWidth() / 2 - background.getWidth() / 2, 
 			Gdx.graphics.getHeight() / 2 - background.getHeight() / 2);
 		this.stage.addActor(background);
+
+		Skin skin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+		Button newGame = new TextButton("New Game", skin);
+		newGame.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+		newGame.setPosition(Gdx.graphics.getWidth() / 2 - BUTTON_WIDTH / 2, Gdx.graphics.getHeight() - BUTTON_HEIGHT);
+		newGame.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("Button pressed");
+				return true;
+			} 
+		});
+		this.stage.addActor(newGame);
+		Gdx.input.setInputProcessor(this.stage);
+
 	}
 
 	@Override
