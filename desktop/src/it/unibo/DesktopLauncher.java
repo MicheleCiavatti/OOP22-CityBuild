e2fb7a601ad59e60a2d064897f8f70f505aa7d72
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-17 16:45:00 +0100

    Renamed MainMenu class

--- desktop/src/it/unibo/DesktopLauncher.java
+++ desktop/src/it/unibo/DesktopLauncher.java
@@ -2,15 +2,15 @@ package it.unibo;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import it.unibo.CityBuild;
+import it.unibo.MainMenu;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(CityBuild.MENU_WIDTH, CityBuild.MENU_HEIGHT);
+		config.setWindowedMode(MainMenu.MENU_WIDTH, MainMenu.MENU_HEIGHT);
 		config.setForegroundFPS(60);
 		config.setTitle("CityBuild");
-		new Lwjgl3Application(new CityBuild(), config);
+		new Lwjgl3Application(new MainMenu(), config);
 	}
 }
