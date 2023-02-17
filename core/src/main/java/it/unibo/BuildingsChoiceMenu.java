4542d0d3c946c1e2b954e30535f71ad241249682
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-17 14:53:17 +0100

    different way in order to apply DRY

--- core/src/main/java/it/unibo/BuildingsChoiceMenu.java
+++ core/src/main/java/it/unibo/BuildingsChoiceMenu.java
@@ -11,39 +11,17 @@ public class BuildingsChoiceMenu implements Screen {
     private Stage stage;
 
     public BuildingsChoiceMenu(){
-        stage = new Stage();
         
-        Texture icon1Texture = new Texture("icon1.png");
-        TextureRegion icon1 = new TextureRegion(icon1Texture);
+    }
 
-        Texture icon2Texture = new Texture("icon2.png");
-        TextureRegion icon2 = new TextureRegion(icon1Texture);
+    private void addButton(float x, float y, float width, float height, String imagePath){
+        Texture iconTexture = new Texture(imagePath);
+        TextureRegion icon = new TextureRegion(iconTexture);
 
-        Texture icon3Texture = new Texture("icon3.png");
-        TextureRegion icon3 = new TextureRegion(icon1Texture);
-
-        ImageButton button1 = new ImageButton(new TextureRegionDrawable(icon1));
-        ImageButton button2 = new ImageButton(new TextureRegionDrawable(icon2));
-        ImageButton button3 = new ImageButton(new TextureRegionDrawable(icon3));
-
-        
-        stage.addActor(button1);
-        stage.addActor(button2);
-        stage.addActor(button3);
-
-        float buttonWidth = Gdx.graphics.getWidth() * 0.2f;
-        float buttonHeight = Gdx.graphics.getHeight() * 0.2f;
-        float spacing = Gdx.graphics.getWidth() * 0.1f;
-
-        button1.setPosition(spacing, spacing);
-        button1.setSize(buttonWidth, buttonHeight);
-
-        button2.setPosition(spacing, spacing);
-        button2.setSize(buttonWidth, buttonHeight);
-
-        button3.setPosition(spacing, spacing);
-        button3.setSize(buttonWidth, buttonHeight);
-
+        ImageButton button = new ImageButton(new TextureRegionDrawable(icon));
+        stage.addActor(button);
+        button.setPosition(x, y);
+        button.setSize(width, height);
     }
     
 }
