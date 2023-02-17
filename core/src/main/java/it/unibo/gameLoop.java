a9c1bdff86d4a6f987885f4fab9726adbde4ea1b
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-17 11:05:44 +0100

    implemented method start

--- core/src/main/java/it/unibo/gameLoop.java
+++ core/src/main/java/it/unibo/gameLoop.java
@@ -1,7 +1,38 @@
 package it.unibo;
 public class gameLoop {
+    private boolean running = true;
+
     public gameLoop(CityBuild cityBuild, BackgroundTask backgroundTask) {
         this.cityBuild = cityBuild;
         this.backgroundTask = backgroundTask;
+    }
+    public void start() {
+        long lastUpdateTime = System.nanoTime();
+        long lastRenderTime = System.nanoTime();
+        long now, updateLength, sleepTime, timeSinceLastRender;
+
+        while (running){
+            now = System.nanoTime();
+            updateLength = now - lastUpdateTime;
+            lastUpdateTime = now;
+            cityBuild.update(updateLength);
+            
+            timeSinceLastRender = now - lastRenderTime;
+            if (timeSinceLastRender > 1_000_000_000 / 60) {
+                lastRenderTime = now;
+                cityBuild.render();
+            }else{
+                sleepTime = (1_000_000_000 - timeSinceLastRender) / 1_000_000;
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+        }
+
+        backgroundTask.start();
+
     }
 }
