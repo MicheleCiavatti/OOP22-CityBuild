9b65c1ca673298c121a5bcf8f537fa0656e58b2f
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-18 15:25:01 +0100

    Renamed GameInterface to GameScreen

--- core/src/main/java/it/unibo/view/GameScreen.java
+++ core/src/main/java/it/unibo/view/GameScreen.java
@@ -1,59 +1,76 @@
 package it.unibo.view;
 
-import java.io.File;
-
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
-public class GameScreen implements Screen{
+public class GameInterface implements ApplicationListener {
 
-    private SpriteBatch batch;
-    private Texture img;
+    private Stage stage;
+    private String selectedBuildingName;
+    private static final String EXTENSION = ".png";
 
-    @Override
-    public void show() {
-        img = new Texture("images" + File.separator + "badlogic.jpg");
-        batch = new SpriteBatch();
+    private void addButton(float x, float y, float width, float height, String imagePath, String buildingName){
+        Texture iconTexture = new Texture(imagePath);
+        TextureRegion icon = new TextureRegion(iconTexture);
+
+        ImageButton button = new ImageButton(new TextureRegionDrawable(icon));
+        button.setName(buildingName);
+        stage.addActor(button);
+        button.setPosition(x, y);
+        button.setSize(width, height);
+
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                selectedBuildingName = buildingName+EXTENSION;
+                System.out.println("Selected building: " + selectedBuildingName);
+            }
+        });
     }
 
     @Override
-    public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-        
-    }
+    public void pause() {}
 
     @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void resume() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-        
-    }
+    public void resume(){}
+    
 
     @Override
     public void dispose() {
-        // TODO Auto-generated method stub
+        stage.dispose(); 
+    }
+
+    @Override
+    public void create() {
+        stage = new Stage();
+        
+        addButton(0, 0, 100, 100, "./desktop/bin/main/badlogic.jpg", "button1");
+        addButton(120, 0, 100, 100, "./desktop/bin/main/badlogic.jpg", "button2");
+        addButton(240, 0, 100, 100, "./desktop/bin/main/badlogic.jpg", "button3");
+
+        Gdx.input.setInputProcessor(stage);
         
     }
-    
+
+    @Override
+    public void render() {
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+        
+    } 
 }
+
+
+
+
+
