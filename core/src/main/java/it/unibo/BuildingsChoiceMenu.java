d8ec8b79f4c85e4cc0c6a3012eab2b2385f90b47
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-02-19 14:33:33 +0100

    when windows resized, button duplicated. fixed problem

--- core/src/main/java/it/unibo/BuildingsChoiceMenu.java
+++ core/src/main/java/it/unibo/BuildingsChoiceMenu.java
@@ -1,10 +1,11 @@
 package it.unibo;
 import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -16,6 +17,7 @@ public class BuildingsChoiceMenu extends ScreenAdapter implements ApplicationLis
     private Stage stage;
     private String selectedBuildingName;
     private static final String EXTENSION = ".png";
+    private boolean isResizing;
 
     private void addButton(float x, float y, float width, float height, String imagePath, String buildingName){
         Texture iconTexture = new Texture(imagePath);
@@ -43,22 +45,68 @@ public class BuildingsChoiceMenu extends ScreenAdapter implements ApplicationLis
 
     @Override
     public void create() {
-        stage = new Stage();
+        /*stage = new Stage();
         
         addButton(0, 0, 100, 100, "./desktop/bin/main/badlogic.jpg", "button1");
-        addButton(120, 0, 100, 100, "./desktop/bin/main/badlogic.jpg", "button2");
-        addButton(240, 0, 100, 100, "./desktop/bin/main/badlogic.jpg", "button3");
+        addButton(0, 110, 100, 100, "./desktop/bin/main/badlogic.jpg", "button2");
+        addButton(0, 220, 100, 100, "./desktop/bin/main/badlogic.jpg", "button3");
 
-        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(stage);*/
+
+
+        
+    stage = new Stage();
+    float buttonWidth = 100;
+    float buttonHeight = 100;
+    float buttonSpacing = 10;
+    float buttonY = (Gdx.graphics.getHeight() - buttonHeight * 3 - buttonSpacing * 2) / 2;
+    
+    addButton((Gdx.graphics.getWidth() - buttonWidth) / 2, buttonY, buttonWidth, buttonHeight, "./desktop/bin/main/badlogic.jpg", "button1");
+    addButton((Gdx.graphics.getWidth() - buttonWidth) / 2, buttonY + buttonHeight + buttonSpacing, buttonWidth, buttonHeight, "./desktop/bin/main/badlogic.jpg", "button2");
+    addButton((Gdx.graphics.getWidth() - buttonWidth) / 2, buttonY + (buttonHeight + buttonSpacing) * 2, buttonWidth, buttonHeight, "./desktop/bin/main/badlogic.jpg", "button3");
+
+    Gdx.input.setInputProcessor(stage);
+}
+
+    @Override
+    public void render() {
+        if (!isResizing) {
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            stage.act(Gdx.graphics.getDeltaTime());
+            stage.draw();
+        }
         
     }
 
     @Override
-    public void render() {
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
+    public void resize(int width, int height) {
+
         
+
+        stage.getViewport().update(width, height, true);
+        float buttonWidth = 100;
+        float buttonHeight = 100;
+        float buttonSpacing = 10;
+        float buttonY = (height - buttonHeight * 3 - buttonSpacing * 2) / 2;
+
+        // Aggiorna la posizione dei bottoni
+        for (Actor actor : stage.getActors()) {
+            if (actor instanceof ImageButton) {
+                ImageButton button = (ImageButton) actor;
+                float buttonX = (width - buttonWidth) / 2;
+                button.setPosition(buttonX, buttonY);
+                buttonY += buttonHeight + buttonSpacing;
+            }
+        }
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
     }
+
+
+
+    
 }
 
 
