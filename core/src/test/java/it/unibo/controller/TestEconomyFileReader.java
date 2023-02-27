ce4ca520cd14d241467348b823b1af3f2a22180b
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-27 15:32:48 +0100

    Minor refactoring in TestEconomyFileReader

--- core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
+++ core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
@@ -6,22 +6,13 @@ import it.unibo.controller.api.EconomyFileReader;
 import it.unibo.controller.impl.EconomyFileReaderImpl;
 import it.unibo.model.api.Resource;
 
-/**A class to test the functioning of {@link it.unibo.controller.api.EconomyFileReader}. */
 public class TestEconomyFileReader {
 
     private final EconomyFileReader economyFileReader = new EconomyFileReaderImpl();
 
-    /**Testing the reading of the house.yml file.
-     * 
-     */
     @Test
     public void testHouse() {
         var tables = economyFileReader.getSimpleEconomyTables(Resource.CITIZEN);
-<<<<<<< HEAD
-        System.out.println(tables);
-        assertNotNull(tables);
-        assertEquals(tables.size(), 3);
-=======
         assertEquals(3, tables.size());
     }
 
@@ -35,6 +26,5 @@ public class TestEconomyFileReader {
     public void testQuantumReactor() {
         var tables = economyFileReader.getAdvancedEconomyTables(Resource.ENERGY);
         assertEquals(3, tables.size());
->>>>>>> feature-building-mac
     }
 }
