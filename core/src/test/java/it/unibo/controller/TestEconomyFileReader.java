3e3589395031d393b278a5012af8ee9c4d4b4dbf
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-02-27 15:31:20 +0100

    Added two methods in TestEconomyFileReader

--- core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
+++ core/src/test/java/it/unibo/controller/TestEconomyFileReader.java
@@ -1,7 +1,6 @@
 package it.unibo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import it.unibo.controller.api.EconomyFileReader;
 import it.unibo.controller.impl.EconomyFileReaderImpl;
@@ -18,7 +17,18 @@ public class TestEconomyFileReader {
     @Test
     public void testHouse() {
         var tables = economyFileReader.getSimpleEconomyTables(Resource.CITIZEN);
-        assertNotNull(tables);
-        assertEquals(tables.size(), 3);
+        assertEquals(3, tables.size());
+    }
+
+    @Test
+    public void testForge() {
+        var tables = economyFileReader.getAdvancedEconomyTables(Resource.METAL);
+        assertEquals(3, tables.size());
+    }
+
+    @Test
+    public void testQuantumReactor() {
+        var tables = economyFileReader.getAdvancedEconomyTables(Resource.ENERGY);
+        assertEquals(3, tables.size());
     }
 }
