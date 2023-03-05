8bb20a19f4d2884aea3f4fdb64abbdb945cdf76d
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-03-05 16:07:04 +0100

    Implemented build() in CityImpl

--- core/src/main/java/it/unibo/model/impl/CityImpl.java
+++ core/src/main/java/it/unibo/model/impl/CityImpl.java
@@ -1,8 +1,10 @@
 package it.unibo.model.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import it.unibo.model.api.City;
+import it.unibo.model.api.Player;
 import it.unibo.model.api.ProductionBuilding;
 import it.unibo.model.api.Resource;
 
@@ -16,11 +18,49 @@ public class CityImpl implements City {
         Resource.ENERGY, 3, Resource.GOLD, 1, Resource.METAL, 2,
         Resource.WATER, 5, Resource.WOOD, 2
     );
+    private static final Map<Resource, Integer> NULL_MAP = Map.of(Resource.CITIZEN, 0);
+    private static final int CITIZENS_TO_ADD = 1;
+    private static final int CITIZENS_TO_LOSE = 2;
 
+    private final Player player;
+    private final List<ProductionBuilding> buildings;
+    private int citizens;
+
+    public CityImpl(final Player player) {
+        this.player = player;
+        this.player.addResources(START_RESOURCES);
+        this.buildings = new ArrayList<>();
+        this.citizens = 0;
+    }
+
+    /**{@inheritDoc} */
     @Override
     public boolean build(ProductionBuilding building) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'build'");
+        if (this.player.spendResources(building.getCostConstruction())) {
+            this.operations(true, building);
+            return true;
+        }
+        return false;
+    }
+
+    /*The Resource.CITIZEN is special because it holds the capacity of the city: therefore, the revenue regarding CITIZEN
+     * must be added immediately in the player resources when the building is built, and if the building providing that revenue 
+     * is destroyed than it must be subtracted immediately in the player resources.
+     */
+    private void operations(final boolean adding, final ProductionBuilding b) {
+        if (adding) {
+            this.buildings.add(b);
+            this.player.addResources(this.checkForCitizens(b.getRevenue()));
+        } else {
+            this.buildings.remove(b);
+            this.player.spendResources(this.checkForCitizens(b.getRevenue()));
+        }
+    }
+
+    private Map<Resource, Integer> checkForCitizens(final Map<Resource, Integer> revenue) {
+        return revenue.containsKey(Resource.CITIZEN)
+            ? Map.of(Resource.CITIZEN, revenue.get(Resource.CITIZEN))
+            : NULL_MAP;
     }
 
     @Override
