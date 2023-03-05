b3db2c7d838ed98f2dcd6bea9c75e01cd055c731
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-03-05 15:52:31 +0100

    Added methods in City

--- core/src/main/java/it/unibo/model/api/City.java
+++ core/src/main/java/it/unibo/model/api/City.java
@@ -1,5 +1,23 @@
-package it.unibo.model.impl;
+package it.unibo.model.api;
 
+import java.util.List;
+import java.util.Map;
+
+/**This interface models the city of the {@link it.unibo.model.api.Player}.
+ * In the MVC, this represents the Model.*/
 public interface City {
     
+    public boolean build(final ProductionBuilding building);
+
+    public void demolish(final ProductionBuilding building);
+
+    public boolean upgrade(final ProductionBuilding building);
+
+    public Map<Resource, Integer> getPlayerResources();
+
+    public int getCitizens();
+
+    public List<ProductionBuilding> getBuildings();
+
+    public void doCycle();
 }
