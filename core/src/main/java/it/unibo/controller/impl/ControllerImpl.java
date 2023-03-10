95c601386be93ec3716f61ffd7479210491ba56a
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-03-10 12:59:13 +0100

    added constructor

--- core/src/main/java/it/unibo/controller/impl/ControllerImpl.java
+++ core/src/main/java/it/unibo/controller/impl/ControllerImpl.java
@@ -1,8 +1,34 @@
 package it.unibo.controller.impl;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import it.unibo.controller.api.Controller;
+import it.unibo.model.api.BuildingFactory;
+import it.unibo.model.api.City;
+import it.unibo.model.api.ProductionBuilding;
+import it.unibo.model.api.Resource;
+import it.unibo.model.impl.BuildingFactoryImpl;
 
 public class ControllerImpl implements Controller {
+
+    private final Set<ProductionBuilding> allBuildings;
+    private final City city;
+    private final BuildingFactory factory;
+
+    public ControllerImpl(final City city) {
+        this.allBuildings = new HashSet<>();
+        final BuildingFactory factory = new BuildingFactoryImpl();
+        Arrays.stream(Resource.values())
+            .flatMap(res -> Stream.of(factory.createSimpleProductionBuilding(res), factory.createAdvancedProductionBuilding(res)))
+            .forEach(allBuildings::add);
+        this.city = city;
+        this.factory = new BuildingFactoryImpl();
+    }
+
+
+
 
     @Override
     public boolean checkResourcesAndBuild(String buildingName) {
