af82c20d70d312cf0b1142f41498da7582a2d12b
Mattia Flamigni - mattia.flamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-03-23 09:10:25 +0100

    created method in order to take resources to increment

--- core/src/main/java/it/unibo/controller/impl/BackgroundTaskImpl.java
+++ core/src/main/java/it/unibo/controller/impl/BackgroundTaskImpl.java
@@ -1,3 +1,32 @@
+package it.unibo.controller.impl;
+
+
+import java.util.Map;
+
+import it.unibo.controller.api.Controller;
+import it.unibo.model.api.Player;
+import it.unibo.model.api.Resource;
+import it.unibo.model.impl.PlayerImpl;
+
 public class BackgroundTaskImpl {
+    //mappa che contiene le risorse da incrementare
     
+    public Map<Resource, Integer> resources;
+
+
+    public void setResources() {
+        final Player player = new PlayerImpl();
+        //ottiene le risorse con le rispettive quantità
+        resources = player.getAllResources();
+    }
+
+    public Map<Resource, Integer> getResources() {
+        if (resources == null) {
+            setResources();
+        }
+        return resources;
+    }
+    
+
+
 }
