185a618ba7296f0fe2d3785f7312af2e8411ba31
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-04-03 10:53:06 -0700

    formatted code

--- core/src/main/java/it/unibo/model/api/Fire.java
+++ core/src/main/java/it/unibo/model/api/Fire.java
@@ -2,10 +2,13 @@ package it.unibo.model.api;
 
 /* An interface that defines methods for a fire. */
 public interface Fire {
-    /* Calculates the intensity of the fire.
-    @return The intensity of the fire. */
+    /*
+     * Calculates the intensity of the fire.
+     * 
+     * @return The intensity of the fire.
+     */
     public int calculateIntensity();
-    
+
     /* Sets the cost of the fire. */
     public void setCost();
 }
