216b68ee4b70d62132d4f6d2b45233607ecd4ed1
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-04-05 02:03:00 -0700

    add methods

--- core/src/main/java/it/unibo/model/api/Fire.java
+++ core/src/main/java/it/unibo/model/api/Fire.java
@@ -9,11 +9,34 @@ public interface Fire {
      * 
      * @throws IllegalArgumentException if the calculation fails.
      */
-    public int calculateIntensity() throws IllegalArgumentException;
+    //public int calculateIntensity() throws IllegalArgumentException;
 
     /**
      * Sets the cost of the fire based on the number of citizens and water resources
      * in the city.
      */
     public void setCost();
+
+    /**
+     * Spends the given amount of gold by the player.
+     *
+     * @param player The player who is spending the gold.
+     * @param cost   The amount of gold to be spent.
+     */
+    public void spendGold(Player player, int cost);
+
+    /**
+     * Performs the fire action.
+     */
+
+    public void performFireAction();
+
+    /**
+     * Returns the cost of the fire.
+     *
+     * @return The cost of the fire.
+     */
+    public int getCost();
+
+    
 }
